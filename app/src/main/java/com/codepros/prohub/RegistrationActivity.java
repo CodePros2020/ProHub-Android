8720770c45a4543f77ceaac5c1842e558d52de77
Logan Junhwi Kim <logan.kim0212@gmail.com>
2020-02-19 11:43:15 -0500

    Registration UI created.

--- app/src/main/java/com/codepros/prohub/RegistrationActivity.java
+++ app/src/main/java/com/codepros/prohub/RegistrationActivity.java
@@ -6,16 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class RegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registration);
     }
 
-    public void goRegistration(View view) {
-        Intent intent = new Intent(this, RegistrationActivity.class);
+
+    public void goMain(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
         this.startActivity(intent);
     }
 }

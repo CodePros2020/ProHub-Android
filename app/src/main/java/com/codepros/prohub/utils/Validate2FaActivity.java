491b00c14d29ccfb5e004331ed3f6bac679e93e4
Kei Mizubuchi <kmizubuc@my.centennialcollege.ca>
2020-02-23 10:09:31 -0500

    Added import library to PropertyHome

--- app/src/main/java/com/codepros/prohub/utils/Validate2FaActivity.java
+++ app/src/main/java/com/codepros/prohub/utils/Validate2FaActivity.java
@@ -14,6 +14,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.codepros.prohub.MainActivity;
+import com.codepros.prohub.PropertyHomeActivity;
 import com.codepros.prohub.R;
 
 import org.json.JSONException;

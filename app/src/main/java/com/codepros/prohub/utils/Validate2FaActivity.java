7c4a79d114d26f665a760abc80ad4ecea5379b2a
Hang (Anna) Li <annlihappy@gmail.com>
2020-03-02 16:16:49 -0500

    after pass 2FA, set is2FA attribute to true

--- app/src/main/java/com/codepros/prohub/utils/Validate2FaActivity.java
+++ app/src/main/java/com/codepros/prohub/utils/Validate2FaActivity.java
@@ -3,6 +3,7 @@ package com.codepros.prohub.utils;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -10,10 +11,14 @@ import android.widget.Toast;
 
 import com.codepros.prohub.PropertyHomeActivity;
 import com.codepros.prohub.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class Validate2FaActivity extends AppCompatActivity {
     private String securityCode;
+    // firebase database objects
+    private DatabaseReference myUserRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +38,15 @@ public class Validate2FaActivity extends AppCompatActivity {
     // go to next page which is the property home page
     private void goNext(){
         if(((EditText) findViewById(R.id.codeEdtx)).getText().toString().equals(securityCode)) {
+            // need to set the user's is2FA property to true
+            SharedPreferences myPref = getSharedPreferences("myUserSharedPref", MODE_PRIVATE);
+            String userPhone = myPref.getString("phoneNum", "");
+            myUserRef = FirebaseDatabase.getInstance().getReference();
+            myUserRef.child("users").child(userPhone).child("is2FA").setValue(true);
+
+            // go to next page
             this.startActivity(new Intent(this, PropertyHomeActivity.class));
-            Toast.makeText(this, "successfully pased the 2FA!", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "successfully passed the 2FA!", Toast.LENGTH_LONG).show();
         }
         else {
             Toast.makeText(this, "incorrect code!", Toast.LENGTH_LONG).show();

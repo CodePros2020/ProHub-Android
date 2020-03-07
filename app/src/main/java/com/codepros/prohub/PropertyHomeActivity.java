f0f0229e8155c1239d9ef49e69583627ba09edd4
Kei Mizubuchi <kmizubuc@my.centennialcollege.ca>
2020-03-07 15:55:37 -0500

    Added PDF Generator

--- app/src/main/java/com/codepros/prohub/PropertyHomeActivity.java
+++ app/src/main/java/com/codepros/prohub/PropertyHomeActivity.java
@@ -1,16 +1,45 @@
 package com.codepros.prohub;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Rect;
+import android.graphics.pdf.PdfDocument;
+import android.graphics.pdf.PdfDocument.*;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.codepros.prohub.model.Property;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.HashMap;
 
 public class PropertyHomeActivity extends AppCompatActivity {
@@ -79,4 +108,200 @@ public class PropertyHomeActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ChatActivity.class);
         this.startActivity(intent);
     }
+
+    ///////////
+    // resource:
+    // generate PDF
+    // https://github.com/rakesh2gnit/export-print-pdf-android
+    // pdf viewer:
+    // need to build in Android App and install into emulator
+    // https://github.com/barteksc/AndroidPdfViewer
+    // export chat history function tentatively put here
+    private static final int PERMISSION_REQUEST_CODE = 100;
+    private String FILENAME;
+
+    public void exportChatHistory(View view){
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkPermission()) {
+                printPdf();
+            } else {
+                requestPermission(); // Code for permission
+            }
+        } else {
+            printPdf();
+        }
+
+    }
+
+    public void printPdf(){
+        // get storage directory path
+        FILENAME = Environment.getExternalStorageDirectory().toString() + "/PDF/" + "Name.pdf";
+
+        // Create New Blank Document
+        Document document = new Document(PageSize.A4);
+
+        // create Directory
+        String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File(root + "/PDF");
+        if(!myDir.exists()) {
+            myDir.mkdirs();
+        }
+
+        // create new document via PDF Writer
+        try {
+            PdfWriter pdfWriter = PdfWriter.getInstance(document, new FileOutputStream(FILENAME));
+
+            document.open();
+
+            // User Define Method
+            addMetaData(document);
+            addTitlePage(document, pdfWriter);
+        } catch(FileNotFoundException e) {
+            e.printStackTrace();
+        } catch(DocumentException e) {
+            e.printStackTrace();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        document.close();
+
+        Toast.makeText(this, "PDF File created! : " + FILENAME, Toast.LENGTH_LONG).show();
+
+        openGeneratedPDF();
+
+    }
+
+    private void openGeneratedPDF() {
+        File file = new File(FILENAME);
+        if(file.exists()) {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            Uri uri = Uri.fromFile(file);
+            intent.setDataAndType(uri, "application/pdf");
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            try {
+                startActivity(intent);
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(PropertyHomeActivity.this, "No Application avaiable to view pdf", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    // Set PDF document Properties
+    public void addMetaData(Document document) {
+        document.addTitle("RESUME");
+        document.addSubject("Person Info");
+        document.addKeywords("Personal,	Education, Skills");
+        document.addAuthor("TAG");
+        document.addCreator("TAG");
+    }
+
+    ///////////////////////////
+
+
+    public void addTitlePage(Document document, PdfWriter pdfWriter) throws DocumentException, IOException {
+        // Font Style for Document
+        Font catFont = new Font(Font.FontFamily.TIMES_ROMAN, 18, Font.BOLD);
+        Font titleFont = new Font(Font.FontFamily.TIMES_ROMAN, 22, Font.BOLD
+                | Font.UNDERLINE, BaseColor.GRAY);
+        Font smallBold = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD);
+        Font normal = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.NORMAL);
+
+        // Start New Paragraph
+        Paragraph prHead = new Paragraph();
+        // Set Font in this Paragraph
+        prHead.setFont(titleFont);
+        // Add item into Paragraph
+        prHead.add("RESUME – Name\n");
+
+        // Create Table into Document with 1 Row
+        PdfPTable myTable = new PdfPTable(1);
+        // 100.0f mean width of table is same as Document size
+        myTable.setWidthPercentage(100.0f);
+
+        // Create New Cell into Table
+        PdfPCell myCell = new PdfPCell(new Paragraph(""));
+        myCell.setBorder(Rectangle.BOTTOM);
+
+        // Add Cell into Table
+        myTable.addCell(myCell);
+
+        prHead.setFont(catFont);
+        prHead.add("\nName1 Name2\n");
+        prHead.setAlignment(Element.ALIGN_CENTER);
+
+        // Add all above details into Document
+        document.add(prHead);
+        document.add(myTable);
+
+        document.add(myTable);
+
+        // Now Start another New Paragraph
+        Paragraph prPersinalInfo = new Paragraph();
+        prPersinalInfo.setFont(smallBold);
+        prPersinalInfo.add("Address 1\n");
+        prPersinalInfo.add("Address 2\n");
+        prPersinalInfo.add("City: SanFran. State: CA\n");
+        prPersinalInfo.add("Country: USA Zip Code: 000001\n");
+        prPersinalInfo
+                .add("Mobile: 9999999999 Fax: 1111111 Email: john_pit@gmail.com \n");
+
+        prPersinalInfo.setAlignment(Element.ALIGN_CENTER);
+
+        document.add(prPersinalInfo);
+        document.add(myTable);
+
+        document.add(myTable);
+
+        Paragraph prProfile = new Paragraph();
+        prProfile.setFont(smallBold);
+        prProfile.add("\n \n Profile : \n ");
+        prProfile.setFont(normal);
+        prProfile
+                .add("\nI am Mr. XYZ. I am Android Application Developer at TAG.");
+
+        prProfile.setFont(smallBold);
+        document.add(prProfile);
+
+        String s = "<pre>this is <em>obviously</em> for <span style=\"text-decoration: underline;\">members</span> <strong>just</strong> <span style=\"background-color: #ff0000;\">going</span> to talk about 5 <span style=\"color: #ff0000;\">minutes </span>spontaneously you need to write down the text of what I say when</pre>";
+
+        XMLWorkerHelper worker = XMLWorkerHelper.getInstance();
+        worker.parseXHtml(pdfWriter, document, new StringReader(s));
+
+        // Create new Page in PDF
+        document.newPage();
+    }
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(PropertyHomeActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(PropertyHomeActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            Toast.makeText(PropertyHomeActivity.this, "Write External Storage permission allows us to save files. Please allow this permission in App Settings.", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(PropertyHomeActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.e("value", "Permission Granted, Now you can use local drive .");
+                } else {
+                    Log.e("value", "Permission Denied, You cannot use local drive .");
+                }
+                break;
+        }
+    }
+
+
+
 }

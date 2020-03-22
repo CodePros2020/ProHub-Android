0dc56861dd52a6464c616b0cefe25ffb7d435c72
Sadia Rashid <sadia.rashid1801@gmail.com>
2020-03-22 09:43:32 -0400

    commit

--- app/src/main/java/com/codepros/prohub/NewsViewActivity.java
+++ app/src/main/java/com/codepros/prohub/NewsViewActivity.java
@@ -5,7 +5,9 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,6 +18,8 @@ import com.codepros.prohub.utils.ChatAdapter;
 import com.codepros.prohub.utils.FirebaseDataseHelper;
 import com.codepros.prohub.utils.NewsAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +30,7 @@ public class NewsViewActivity extends AppCompatActivity {
     public NewsAdapter newsAdapter;
     public List<News> newsList=new ArrayList<News>();
     public FloatingActionButton btn_add;
+    private DatabaseReference myPropRef;
 
 
     @Override
@@ -33,7 +38,17 @@ public class NewsViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_view);
 
+        //Shared pref
+        SharedPreferences sharedPreferences = getSharedPreferences("myUserSharedPref", Context.MODE_PRIVATE);
+        String userPhone= sharedPreferences.getString("keyName", "defaultValue");
+
         //
+        myPropRef = FirebaseDatabase.getInstance().getReference();
+
+        myRole = myPreference.getString("myRole", "");
+
+        //
+
         btn_add=findViewById(R.id.btn_add_news);
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
